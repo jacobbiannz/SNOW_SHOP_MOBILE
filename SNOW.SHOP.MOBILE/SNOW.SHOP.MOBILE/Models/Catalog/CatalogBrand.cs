db99144defdb79ade3de8fb08d4ee58ecffd3530
jacobbiannz <jacob.bian@gmail.com>
2017-09-19 16:53:04 +1200

    add model and servcie

---
+++ SNOW.SHOP.MOBILE/SNOW.SHOP.MOBILE/Models/Catalog/CatalogBrand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNOW.SHOP.MOBILE.Models.Catalog
+{
+    public class CatalogBrand
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+
+        public override string ToString()
+        {
+            return Brand;
+        }
+    }
+}
---
+++ SNOW.SHOP.MOBILE/SNOW.SHOP.MOBILE/Services/Catalog/ICatalogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SNOW.SHOP.MOBILE.Models.Catalog;
+using System.Collections.ObjectModel;
+
+namespace SNOW.SHOP.MOBILE.Services.Catalog
+{
+    public interface ICatalogService
+    {
+        Task<ObservableCollection<CatalogBrand>> GetCatalogBrandAsync();
+        Task<ObservableCollection<CatalogProduct>> FilterAsync(int catalogBrandId, int catalogTypeId);
+        Task<ObservableCollection<CatalogCategory>> GetCatalogTypeAsync();
+        Task<ObservableCollection<CatalogProduct>> GetCatalogAsync();
+    }
+}
