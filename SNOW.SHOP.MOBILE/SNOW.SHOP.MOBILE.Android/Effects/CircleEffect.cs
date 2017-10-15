7a75351f0b4bc9c4c79bd600aa7a3a7682082656
jacobbiannz <jacob.bian@gmail.com>
2017-10-15 16:56:07 +1300

    Phase 1 done

---
+++ SNOW.SHOP.MOBILE/SNOW.SHOP.MOBILE.Android/Effects/CircleEffect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+using SNOW.SHOP.MOBILE.Droid.Effects;
+using Android.Graphics;
+
+
+
+[assembly: ExportEffect(typeof(CircleEffect), "CircleEffect")]
+namespace SNOW.SHOP.MOBILE.Droid.Effects
+{
+    public class CircleEffect : BaseContainerEffect
+    {
+        private ViewOutlineProvider _originalProvider;
+
+        protected override bool CanBeApplied()
+        {
+            return Container != null && (int)global::Android.OS.Build.VERSION.SdkInt >= 21;
+        }
+
+        protected override void OnAttachedInternal()
+        {
+            _originalProvider = Container.OutlineProvider;
+            Container.OutlineProvider = new CircleOutlineProvider();
+            Container.ClipToOutline = true;
+        }
+
+        protected override void OnDetachedInternal()
+        {
+            Container.ClipToOutline = false;
+            Container.OutlineProvider = _originalProvider;
+        }
+
+        private class CircleOutlineProvider : ViewOutlineProvider
+        {
+            public override void GetOutline(Android.Views.View view, Outline outline)
+            {
+                double width = view.Width;
+                double height = view.Height;
+
+                if (width <= 0 || height <= 0)
+                {
+                    return;
+                }
+
+                double min = Math.Min(width, height);
+                var radius = (float)(min / 2.0);
+
+                var layerX = width > min ? (width - min) / 2 : 0;
+                var layerY = height > min ? (height - min) / 2 : 0;
+
+                outline.SetRoundRect(new Rect((int)layerX, (int)layerY, (int)(layerX + min), (int)(layerY + min)), radius);
+            }
+        }
+    }
+}
